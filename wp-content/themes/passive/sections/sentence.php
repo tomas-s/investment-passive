15763f7f670d7cb0a4bd31c5c9de7adf7045c5f3
tomas-s <tomas.slizik@gmail.com>
2020-06-23 14:46:57 +0200

    text change

--- wp-content/themes/passive/sections/sentence.php
+++ wp-content/themes/passive/sections/sentence.php
@@ -1,5 +1,5 @@
 <section class="sentence p-top-bottom">
   <div class="container">
-    <h2> <span>Zvacsi pasivny prijem az o 30%</span></h2>
+    <h2> <span>Zväčší pasívny príjem až o 30%</span></h2>
   </div>
 </section>
