754dd6e49e7501ae27137ccc8e6fdad42d618d8f
Roman Stix <roman.stix@digitalsunray.com>
2020-06-23 18:10:44 +0200

    favicon added, frontpage responsive

--- wp-content/themes/passive/header.php
+++ wp-content/themes/passive/header.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset='<?php bloginfo('charset'); ?>'>
   <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/robot.png">
   <script src="https://kit.fontawesome.com/f130b21045.js" crossorigin="anonymous"></script>
   <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
     rel="stylesheet">
