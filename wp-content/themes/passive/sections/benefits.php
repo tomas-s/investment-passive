eaa4066abd0bfd443e964431544a9041d94e5bab
tomas-s <tomas.slizik@gmail.com>
2020-06-23 14:48:31 +0200

    fix

--- wp-content/themes/passive/sections/benefits.php
+++ wp-content/themes/passive/sections/benefits.php
@@ -23,15 +23,18 @@
     <div class="benefits-items-text">
       <div class="benefits-item">
         <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/icons/sunbed.svg" alt="upto icon"> -->
-        <p>HILO je náš robot, ktorý vás zbaví neustáleho nastavovania ťažby a tým sa budete môcť venovať svojmu biznisu,
-          rodine, priateľom, koníčkom a pod. (KOMFORT)</p>
+        <p>Komfort</p>
+        <p>HILO je robot, ktorý za Vás nastaví ťažbu. Vy sa budete môcť venovať svojmu biznisu,
+          rodine, priateľom, koníčkom</p>
       </div>
       <div class="benefits-item">
         <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/icons/time.svg" alt="upto icon"> -->
-        <p>HILO je nezastaviteľný, beží 24/7 (NEZASTAVITELNY)</p>
+        <p>Nepretržitá prevádzka</p>
+        <p>HILO je nezastaviteľný, beží 24/7</p>
       </div>
       <div class="benefits-item">
         <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/icons/up_to.png" alt="upto icon"> -->
+        <p>Efektivitu</p>
         <p>HILO konštantne dosahuje až o 30% lepšie výsledky oproti manuálnemu nastavovaniu ťažby</p>
       </div>
     </div>
