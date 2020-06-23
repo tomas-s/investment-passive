8740ad9e52aa15b71a271eebc19adac7fc97e518
Roman Stix <roman.stix@digitalsunray.com>
2020-06-23 14:56:01 +0200

    benefitts responsive

--- wp-content/themes/passive/sections/benefits.php
+++ wp-content/themes/passive/sections/benefits.php
@@ -19,16 +19,19 @@
     </div>
     <div class="benefits-items-text">
       <div class="benefits-item">
-        <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/icons/sunbed.svg" alt="upto icon"> -->
+        <img class='mobile-only sunbed' src="<?php echo get_template_directory_uri(); ?>/images/icons/sunbed.svg"
+          alt="upto icon">
         <p>HILO je náš robot, ktorý vás zbaví neustáleho nastavovania ťažby a tým sa budete môcť venovať svojmu biznisu,
           rodine, priateľom, koníčkom a pod. (KOMFORT)</p>
       </div>
       <div class="benefits-item">
-        <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/icons/time.svg" alt="upto icon"> -->
+        <img class='mobile-only time' src="<?php echo get_template_directory_uri(); ?>/images/icons/time.png"
+          alt="upto icon">
         <p>HILO je nezastaviteľný, beží 24/7 (NEZASTAVITELNY)</p>
       </div>
       <div class="benefits-item">
-        <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/icons/up_to.png" alt="upto icon"> -->
+        <img class='mobile-only upto' src="<?php echo get_template_directory_uri(); ?>/images/icons/up_to.png"
+          alt="upto icon">
         <p>HILO konštantne dosahuje až o 30% lepšie výsledky oproti manuálnemu nastavovaniu ťažby</p>
       </div>
     </div>
