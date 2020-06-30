b3424b626ac439769c9354f37578cbcc7efe725f
Roman Stix <roman.stix@digitalsunray.com>
2020-06-30 18:48:12 +0200

    facebook hidden

--- wp-content/themes/passive/footer.php
+++ wp-content/themes/passive/footer.php
@@ -3,7 +3,7 @@
     <div class="logo-socials">
       <h3>Hilo Investment</h3>
       <a href="mailto:hilo.investment@gmail.com"><i class="far fa-envelope"></i></a>
-      <a href="https://www.facebook.com/Hilo-Investment-104624297973502"><i class="fab fa-facebook-f"></i></a>
+      <!-- <a href="https://www.facebook.com/Hilo-Investment-104624297973502"><i class="fab fa-facebook-f"></i></a> -->
       <!-- <a href=""><i class="fab fa-instagram"></i></a> -->
       <!-- <a href=""><i class="fab fa-telegram"></i></a> -->
     </div>
