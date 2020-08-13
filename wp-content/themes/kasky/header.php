cf3077e5c372b77eee6158d9d800107686a72347
Roman Stix <roman.stix@digitalsunray.com>
2020-08-13 18:14:41 +0200

    logo goes to homepage

--- wp-content/themes/kasky/header.php
+++ wp-content/themes/kasky/header.php
@@ -11,24 +11,26 @@
   <header class="header">
     <div class="container">
       <div class="nav">
-      <div id="menu-toggle">
-        <div id="hamburger">
-          <span></span>
-          <span></span>
-          <span></span>
+        <div id="menu-toggle">
+          <div id="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div id="cross">
+            <span></span>
+            <span></span>
+          </div>
         </div>
-        <div id="cross">
-          <span></span>
-          <span></span>
-        </div>
-      </div>
         <nav>
           <?php wp_nav_menu(array(
             'theme_location' => 'mainMenu'
           ))
           ?>
         </nav>
-        <div class="logo">Kasky</div>
+        <a href="/" class="logo">
+          Kasky
+        </a>
         <div class="socials">
           <a href="https://www.facebook.com/milan.kasala"><i class="fa fa-facebook-f"></i></a>
           <a href="https://www.instagram.com/kasky_12/"><i class="fa fa-instagram"></i></a>
